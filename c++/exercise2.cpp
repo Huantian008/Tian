83f5d8672289663826788a2823190020c1c718d7
Ruence <huantian008@outlook.com>
2025-08-03 20:57:42 +0800

    这一次我完成了一个简单的学生名册管理系统，我遇到的难点是有关vector的遍历 以及如何创建一些临时的变量来接受用户的数据

--- c++/exercise2.cpp
+++ c++/exercise2.cpp
@@ -22,7 +22,6 @@ int main(){
     while (true)
     {
         std::cout<<"学生名册管理系统"<<std::endl;
-        
         std::cout<<"1.添加新学生"<<std::endl;
         std::cout<<"2.显示所有学生"<<std::endl;
         std::cout<<"3.退出程序"<<std::endl;
@@ -30,14 +29,45 @@ int main(){
         std::cin>>number;
         if (number==1)
         {
-            std::cout<<"请依次输入新学生的姓名、学号、分数。"<<std::endl;
-            Student student;
-            student.name;
-            student.score;
-            student.studentID;
+           std::string tempName;
+           int tempID;
+           double tempScore;
+           std::cout<<"请输入学生姓名：";
+           std::cin>>tempName;
+           std::cout<<"请输入学生学号：";
+           std::cin>>tempID;
+           std::cout<<"请输入学生分数：";
+           std::cin>>tempScore;
+           Student newstudent;
+           newstudent.name=tempName;
+           newstudent.score=tempScore;
+           newstudent.studentID=tempID;
+           roster.push_back(newstudent);
+           std::cout<<"学生信息添加成功！\n";
+
+        }else if(number==2){
+            if(roster.size()==0){
+                std::cout<<"当前名册为空\n";
+            }else{
+                for (size_t i = 0; i < roster.size(); ++i)
+                {
+                    
+                    roster[i].displayInfo();
+                }
+            }
+                
+
+                }
+                else if(number==3){
+                    std::cout<<"感谢使用，再见";
+                    break;
+
+                }
+            }
+            return 0;
+
         }
         
-    }
     
-    return 0;
-}
+    
+    
