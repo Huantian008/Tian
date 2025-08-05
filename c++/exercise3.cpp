c18082a8abb25c62c15002cbf418a0f6c80fd16d
Ruence <huantian008@outlook.com>
2025-08-05 11:11:48 +0800

    8.4接触了最基本的算法 想知道一堆数里面，哪个最大，可以先选一个数字出来，然后把其余的数字和它比大小，大的就成为新的数字，可以用for循环遍历 for可以有一个const auto&

---
+++ c++/exercise3.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<vector>
+#include<string>
+int main(){
+   std::vector<double> scores;
+   double number;
+   while(true){
+    std::cout<<"请输入成绩\n";
+    std::cin>>number;
+    
+    if (number==-1)
+    {
+        break;
+    }
+    scores.push_back(number);
+   }
+    if(scores.empty()){
+        std::cout<<"没有输入任何成绩\n";
+    }else{
+
+        double maxScore=scores[0];
+        for (const auto&score:scores)
+        {
+            if(score>maxScore){
+                maxScore=score;
+            }
+        }
+        std::cout<<"最高成绩是："<<maxScore<<std::endl;
+        
+    }
+    return 0;
+}
